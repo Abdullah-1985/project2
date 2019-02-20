f84774ef055309b1dc3237a6ad221a2d9d84df7d
Abdullah-1985 <sheble1985@gmail.com>
2019-02-20 16:04:56 +0300

    Updates

---
+++ precache-manifest.0bcf8bc08eaf82169197d74786d35a21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca87220009693c4e0b53",
+    "url": "/project2/static/css/main.93b6cfeb.chunk.css"
+  },
+  {
+    "revision": "ca87220009693c4e0b53",
+    "url": "/project2/static/js/main.ca872200.chunk.js"
+  },
+  {
+    "revision": "1015b631978e0d766ba7",
+    "url": "/project2/static/js/runtime~main.1015b631.js"
+  },
+  {
+    "revision": "fa21e20625b60d3bb41e",
+    "url": "/project2/static/css/2.9f0bdae8.chunk.css"
+  },
+  {
+    "revision": "fa21e20625b60d3bb41e",
+    "url": "/project2/static/js/2.fa21e206.chunk.js"
+  },
+  {
+    "revision": "d8cdd99104766d28978715eab70a316f",
+    "url": "/project2/index.html"
+  }
+];
